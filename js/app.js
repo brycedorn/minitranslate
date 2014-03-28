e944c602efcd873a059b8199814389d984420052
Bryce Dorn <brycedorn@gmail.com>
2014-03-28 16:23:46 -0500

    makes all buttons links

--- js/app.js
+++ js/app.js
@@ -16,10 +16,10 @@ function update_lib(i) {
   $("#library").empty();
   if(i != -1) library.splice(i,1);
   content = $.map(library, function(entry,i) {
-    return "<tr><td><h4>" + entry.w + "</h4></td><td><span style='padding:8px' class='glyphicon glyphicon-arrow-right'/></td><td><h4>" + entry.r + "</h4></td><td><h4 align='right'><button class='btn btn-warning' onclick=update_lib("+i+")>Remove</button></h4></td></tr>";
+    return "<tr><td><h4>" + entry.w + "</h4></td><td><span style='padding:8px' class='glyphicon glyphicon-arrow-right'/></td><td><h4>" + entry.r + "</h4></td><td><h4 align='right'><button class='btn btn-warning' onclick=update_lib("+i+")><a>Remove</a></button></h4></td></tr>";
   });
   $("#library").append(content.join(""));
-  $("#library").append("<tr><td><input type='text' class='form-control word-input' placeholder='Add a word'></input></td><td><span style='padding:8px' class='glyphicon glyphicon-arrow-right'/></td><td><input type='text' class='form-control replace-input' placeholder='Add its translation'></input></td><td><h4 align='right'><button class='btn btn-large btn-info add' onclick='add_word($(this))'>Add</button></h4></td></tr>");
+  $("#library").append("<tr><td><input type='text' class='form-control word-input' placeholder='Add a word'></input></td><td><span style='padding:8px' class='glyphicon glyphicon-arrow-right'/></td><td><input type='text' class='form-control replace-input' placeholder='Add its translation'></input></td><td><h4 align='right'><button class='btn btn-large btn-info add' onclick='add_word($(this))'><a>Add</a></button></h4></td></tr>");
 }
 
 function add_word(div) {
