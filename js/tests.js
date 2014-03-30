79b9b9107ebe6a4e1ba4a6bd44b921cac9b36b74
Bryce Dorn <brycedorn@gmail.com>
2014-03-30 18:17:45 -0500

    finishes testing, still need to find a way to test dynamic events

--- js/tests.js
+++ js/tests.js
@@ -22,27 +22,25 @@ test("should stop if the div doesn't exist", function() {
 test("should stop if the library is empty", function() {
   var empty_lib = [];
   ok(empty_lib.length == 0, "Passed!" );
-  mt(empty_lib,$("#test-static"));
 });  
 
 test("should work in static case", function() {
   ok($("#test-static").attr('class') == "mt-translate", "Passed!" );
-  ok($("#test-static").children()[0].text() == "Goodbye static World!", "Passed!" );
+  ok($("#test-static").children().text() == "Goodbye static Mars!", "Passed!" );
 });
 
 test("should work in dynamic case", function() {
-  ok($("#test-watch").attr('class') == ".mt-watch", "Passed!" );
-  ok($("#test-watch").children().text() == "Hello World!", "Passed!" );
-  // mt(library,$("#test-watch"));
-  // ok($("#test-watch").children().text() == "/\bGoodbye\b/g World!", "Passed!" );
+  // Requires keyUp event, but works same as static case.
+  // var keyVal = "A";
+  // $("#mt-input").trigger ( {
+  //     type: 'keypress', keyCode: keyVal, which: keyVal, charCode: keyVal
+  // } );
+  ok($("#mt-input").val() == "Hello World!", "Passed!" );
 });
 
 test("should ignore those we want to ignore", function() {
-  ok($("#test-ignore").attr('class') == ".mt-watch", "Passed!" );
-  ok($("#test-ignore").children('h2').attr('class') == ".mt-ignore", "Passed!" );
-  
-  mt(library,$("#test-ignore"));
-
+  ok($("#test-ignore").attr('class') == "mt-translate", "Passed!" );
+  ok($("#test-ignore").children('h2').attr('class') == "mt-ignore", "Passed!" );
   // ok($("#test-ignore").children('h1').text() == "Goodbye Mars!", "Passed!" );
   ok($("#test-ignore").children('h2').text() == "Hello World, please ignore me!", "Passed!" );
 
@@ -52,4 +50,8 @@ test("should ignore those we want to ignore", function() {
 
 test("should apply to a lot of children", function() {
   ok(0==0, "Passed!" );
+});
+
+test("should work with weird capitalizations", function() {
+  ok($("#test-caps").children('h1').text() == "GooDbye mArS!" );
 });
