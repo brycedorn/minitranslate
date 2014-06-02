ed0e334b23d1e0fc5b42239e36c92d56e747fa49
Bryce Dorn <brycedorn@gmail.com>
2014-06-02 14:31:40 -0500

    spring cleaning

--- js/minitranslate.js
+++ js/minitranslate.js
@@ -10,35 +10,44 @@
  * The MIT license.
  */
 function mt(mt_lib, div) {
-  if($(div).length > 0 && (mt_lib.length > 0 && $(div).attr("class") != "mt-ignore")) {
+  var i,j,k,txt,txt_arr,tmp,punct,capitals;
+  if($(div).length > 0 && (mt_lib.length > 0 && $(div).attr("class") !== "mt-ignore")) {
     // Children!
     if($(div).children().length > 0) {
       for(var j = 0; j < $(div).children().length; j++) {
-        if($($(div).children()[j]).attr("class") != "mt-ignore") {
+        if($($(div).children()[j]).attr("class") !== "mt-ignore") {
           // Shouldn't be an input div, so get text (not val)
-          var txt = $($(div).children()[j]).text();
+          txt = $($(div).children()[j]).text();
 
           // Find where punctuation was so we can re-apply at end
-          var tmp = txt.split(" ");
+          tmp = txt.split(" ");
 
           // Only do this if there is punctuation
-          var punct = [];
-          for(var i = 0; i < tmp.length; i++)
-            for(var k = 0; k < tmp[i].length; k++)
+          punct = [];
+          for(i = 0; i < tmp.length; i++) {
+            for(k = 0; k < tmp[i].length; k++) {
               // Word at index i has punctuation at end of it.
-              if(tmp[i].charAt(k) === "!" || tmp[i].charAt(k) === "?" || tmp[i].charAt(k) === "," || tmp[i].charAt(k) === ".") punct.push(new pun(tmp[i].charAt(k), i));
+              if(tmp[i].charAt(k) === "!" || tmp[i].charAt(k) === "?" || tmp[i].charAt(k) === "," || tmp[i].charAt(k) === ".") {
+                punct.push(new pun(tmp[i].charAt(k), i));
+              }
+            }
+          }
 
           // Array of tokens without punctuation
           txt = txt.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-          var txt_arr = txt.split(" ");
+          txt_arr = txt.split(" ");
 
           // Iterate through library
           for(i = 0; i < mt_lib.length; i++)
             for(k = 0; k < txt_arr.length; k++) {
-              var capitals = detect_capitals(txt_arr[k]);
-              if(txt_arr[k].toLowerCase() === mt_lib[i].w.toLowerCase()) txt_arr[k] = apply_capitals(mt_lib[i].r, capitals)
+              capitals = detect_capitals(txt_arr[k]);
+              if(txt_arr[k].toLowerCase() === mt_lib[i].w.toLowerCase()) {
+                txt_arr[k] = apply_capitals(mt_lib[i].r, capitals)
+              }
             }
-          for(i = 0; i < punct.length; i++) txt_arr[punct[i].idx] += punct[i].c;
+          for(i = 0; i < punct.length; i++) {
+            txt_arr[punct[i].idx] += punct[i].c;
+          }
 
           // All words are applied
           txt = txt_arr.join(" ");
@@ -50,14 +59,14 @@ function mt(mt_lib, div) {
     } else { // No kids :(
       // If it's an input, get val(), otherwise get text()
       if($(div).attr("id") === "mt-output") {
-        var txt = $(div).val();
-      } else { var txt = $(div).text(); }
+        txt = $(div).val();
+      } else { txt = $(div).text(); }
 
       // Find where punctuation was so we can re-apply at end
-      var tmp = txt.split(" ");
-      var punct = [];
-      for(var i = 0; i < tmp.length; i++) {
-        for(var k = 0; k < tmp[i].length; k++) {
+      tmp = txt.split(" ");
+      punct = [];
+      for(i = 0; i < tmp.length; i++) {
+        for(k = 0; k < tmp[i].length; k++) {
           if(tmp[i].charAt(k) === "!" || tmp[i].charAt(k) === "?" || tmp[i].charAt(k) === "," || tmp[i].charAt(k) === ".") {
             punct.push(new pun(tmp[i].charAt(k), i));
           }
@@ -66,12 +75,12 @@ function mt(mt_lib, div) {
 
       // Array of tokens without punctuation
       txt = txt.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-      var txt_arr = txt.split(" ");
+      txt_arr = txt.split(" ");
 
       // Iterate through library
-      for(var i = 0; i < mt_lib.length; i++) {
-        for(var j = 0; j < txt_arr.length; j++) {
-          var capitals = detect_capitals(txt_arr[j]);
+      for(i = 0; i < mt_lib.length; i++) {
+        for(j = 0; j < txt_arr.length; j++) {
+          capitals = detect_capitals(txt_arr[j]);
           if(txt_arr[j].toLowerCase() === mt_lib[i].w.toLowerCase()) txt_arr[j] = apply_capitals(mt_lib[i].r, capitals)
         }
       }
