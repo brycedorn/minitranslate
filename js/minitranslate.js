197318d8f3231b2a5e6513bb574dcd9c9e668f12
Bryce Dorn <brycedorn@gmail.com>
2014-06-07 15:46:13 -0500

    refactored & added mt-patient feature

--- js/minitranslate.js
+++ js/minitranslate.js
@@ -4,17 +4,18 @@
  * minitranslate.herokuapp.com
  *
  * @version
- * 0.7.1 (June 2 2014)
+ * 0.8.0 (June 7 2014)
  *
  * @license
  * The MIT license.
  */
 function mt(mt_lib, div) {
-  var i,j,k,txt,txt_arr,tmp,punct,capitals;
+  var i,j,txt,txt_arr,tmp,punct,capitals;
   if($(div).length > 0 && (mt_lib.length > 0 && $(div).attr("class") !== "mt-ignore")) {
+    
     // Children!
     if($(div).children().length > 0) {
-      for(var j = 0; j < $(div).children().length; j++) {
+      for(j = 0; j < $(div).children().length; j++) {
         if($($(div).children()[j]).attr("class") !== "mt-ignore") {
           // Shouldn't be an input div, so get text (not val)
           txt = $($(div).children()[j]).text();
@@ -23,29 +24,15 @@ function mt(mt_lib, div) {
           tmp = txt.split(" ");
 
           // Only do this if there is punctuation
-          punct = [];
-          for(i = 0; i < tmp.length; i++) {
-            for(k = 0; k < tmp[i].length; k++) {
-              // Word at index i has punctuation at end of it.
-              if(tmp[i].charAt(k) === "!" || tmp[i].charAt(k) === "?" || tmp[i].charAt(k) === "," || tmp[i].charAt(k) === ".") {
-                punct.push(new Pun(tmp[i].charAt(k), i));
-              }
-            }
-          }
+          punct = get_punct(tmp);
 
           // Array of tokens without punctuation
           txt = txt.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g, "");
           txt_arr = txt.split(" ");
 
           // Iterate through library
-          for(i = 0; i < mt_lib.length; i++) {
-            for(k = 0; k < txt_arr.length; k++) {
-              capitals = detect_capitals(txt_arr[k]);
-              if(txt_arr[k].toLowerCase() === mt_lib[i].w.toLowerCase()) {
-                txt_arr[k] = apply_capitals(mt_lib[i].r, capitals);
-              }
-            }
-          }
+          iterate_lib(txt_arr, capitals, mt_lib);
+
           for(i = 0; i < punct.length; i++) {
             txt_arr[punct[i].idx] += punct[i].c;
           }
@@ -65,28 +52,17 @@ function mt(mt_lib, div) {
 
       // Find where punctuation was so we can re-apply at end
       tmp = txt.split(" ");
-      punct = [];
-      for(i = 0; i < tmp.length; i++) {
-        for(k = 0; k < tmp[i].length; k++) {
-          if(tmp[i].charAt(k) === "!" || tmp[i].charAt(k) === "?" || tmp[i].charAt(k) === "," || tmp[i].charAt(k) === ".") {
-            punct.push(new Pun(tmp[i].charAt(k), i));
-          }
-        }
-      }
+
+      // Only do this if there is punctuation
+      punct = get_punct(tmp);
 
       // Array of tokens without punctuation
       txt = txt.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g, "");
       txt_arr = txt.split(" ");
 
       // Iterate through library
-      for(i = 0; i < mt_lib.length; i++) {
-        for(j = 0; j < txt_arr.length; j++) {
-          capitals = detect_capitals(txt_arr[j]);
-          if(txt_arr[j].toLowerCase() === mt_lib[i].w.toLowerCase()) {
-            txt_arr[j] = apply_capitals(mt_lib[i].r, capitals);
-          }
-        }
-      }
+      iterate_lib(txt_arr, capitals, mt_lib);
+
       for(i = 0; i < punct.length; i++) {
         txt_arr[punct[i].idx] += punct[i].c;
       }
@@ -104,6 +80,30 @@ function mt(mt_lib, div) {
   }
 }
 
+function iterate_lib(t, capitals, mt_lib) {
+  for(var i = 0; i < mt_lib.length; i++) {
+    for(var j = 0; j < t.length; j++) {
+      capitals = detect_capitals(t[j]);
+      if(t[j].toLowerCase() === mt_lib[i].w.toLowerCase()) {
+        t[j] = apply_capitals(mt_lib[i].r, capitals);
+      }
+    }
+  }
+}
+
+function get_punct(tmp) {
+  var p = [];
+  for(var i = 0; i < tmp.length; i++) {
+    for(var j = 0; j < tmp[i].length; j++) {
+      // Word at index i has punctuation at end of it.
+      if(tmp[i].charAt(j) === "!" || tmp[i].charAt(j) === "?" || tmp[i].charAt(j) === "," || tmp[i].charAt(j) === ".") {
+        p.push(new Pun(tmp[i].charAt(j), i));
+      }
+    }
+  }
+  return p;
+}
+
 function detect_capitals(word) {
   var detect = [];
   for(var i = 0; i < word.length; i++) {
@@ -118,22 +118,16 @@ function detect_capitals(word) {
 
 function apply_capitals(word, capitals) {
   var ret = "";
-  if(word.length >= capitals.length) {
-    for(var i = 0; i < capitals.length; i++) {
-      if(capitals[i]) {
-        ret += word.charAt(i).toUpperCase();
-      } else {
-        ret += word.charAt(i).toLowerCase();
-      }
-    ret += word.substr(i, word.length - 1)
-  } else {
-    for(var i = 0; i < word.length; i++) {
-      if(capitals[i]) {
-        ret += word.charAt(i).toUpperCase();
-      } else {
-        ret += word.charAt(i).toLowerCase();
-      }
+  var end = (word.length >= capitals.length) ? capitals.length : word.length;
+  for(var i = 0; i < end; i++) {
+    if(capitals[i]) {
+      ret += word.charAt(i).toUpperCase();
+    } else {
+      ret += word.charAt(i).toLowerCase();
     }
+  }
+  if(word.length >= capitals.length) { 
+    ret += word.substr(i, word.length - 1);
   }
   return ret;
 }
@@ -147,20 +141,26 @@ function Pun(c, i) {
 function mt_translate(mt_lib) {
   $(".mt-translate").each(function(i, div) {
     mt(mt_lib, div);
-  })
+  });
 }
 
 // Dynamic
 function mt_watch(mt_lib, inp, out) {
   $(inp).keyup(function() {
-    $(out).val($(this).val());
+    if(!inp.hasClass('mt-patient')) {
+      $(out).val($(this).val());
+      mt(mt_lib, $(out));
+    }
+  });
+  $("#mt-button").click(function() {
+    $(out).val($(inp).val());
     mt(mt_lib, $(out));
-  })
+  });
 }
 
 // Validate
-if($("#mt-input").length > 0 && !$("#mt-output").length > 0) { console.log("MT-DEBUG: Input detected but no Output. Check your input IDs"); }
-if(!$("#mt-input").length > 0 && $("#mt-output").length > 0) { console.log("MT-DEBUG: Output detected but no Input. Check your input IDs"); }
+if($("#mt-input").length > 0 && $("#mt-output").length === 0) { console.log("MT-DEBUG: Input detected but no Output. Check your input IDs"); }
+if($("#mt-input").length === 0 && $("#mt-output").length > 0) { console.log("MT-DEBUG: Output detected but no Input. Check your input IDs"); }
 
 // Instantiate
 mt_watch(mt_lib, $("#mt-input"), $("#mt-output"));
